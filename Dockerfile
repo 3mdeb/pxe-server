4376191d5eb0572373364d8211cff9d18cce71ca
Piotr Król <piotr.krol@3mdeb.com>
2016-06-26 16:57:33 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM debian:jessie
+MAINTAINER Piotr Król <piotr.krol@3mdeb.com>
+RUN apt-get update && apt-get install -y \
+  apt-utils \
+  git \
+  vim \
+  tmux \
+  python \
+  python-dev \
+  ntpdate \
+  ca-certificates \
+  tftpd-hpa
+
+EXPOSE 69/udp
---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+NETBOOT_URL=http://ftp.nl.debian.org/debian/dists/jessie/main/installer-i386/current/images/netboot/netboot.tar.gz
+NETBOOT_DIR=netboot
+NETBOOT_TAR=netboot.tar.gz
+
+if [ ! -d  $NETBOOT_DIR ]; then
+	mkdir -p $NETBOOT_DIR
+	wget $NETBOOT_URL
+	tar xvf $NETBOOT_TAR -C $NETBOOT_DIR
+	rm $NETBOOT_TAR
+fi
+
+docker build -t 3mdeb/pxe-server .
+
+if [ $? -ne 0 ]; then
+    echo "ERROR: Unable to build container"
+    exit 1
+fi
+
+cd ..
+docker run -v -p 0.0.0.0:69:69 ${PWD}/${NETBOOT_DIR}:/srv/tftp -t -i 3mdeb/pxe-server
